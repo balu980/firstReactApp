cccdda73146cef3cf3ca2d355d74e6ca1dbf9824
balu980 <balu980@gmail.com>
2018-06-28 07:32:34 -0700

    Error handling

--- src/actions/fetchActions.js
+++ src/actions/fetchActions.js
@@ -1,5 +1,5 @@
-export function fetchGit(querry) {
-    var urlParams = "language:"+querry.lang+"+topic:"+querry.topic
+export function fetchGitProject(querry) {
+    var urlParams = querry.text+"+language:"+querry.lang+"+topic:"+querry.topic
     return function(dispatch) {
         /*
         to add a loading text
@@ -9,6 +9,26 @@ export function fetchGit(querry) {
            search the git with the REST API
         */
         fetch("https://api.github.com/search/repositories?q="+urlParams)
+            .then(response => response.json())
+            .then((response) => {
+                dispatch({type: "FETCH_GIT_FULFILLED", payload: response})
+            })
+            .catch((err) => {
+                dispatch({type: "FETCH_GIT_REJECTED", payload: err})
+            })
+    }
+}
+export function fetchGitCode(querry) {
+    var urlParams = querry.text+"+in:file+repo:"+querry.topic
+    return function(dispatch) {
+        /*
+        to add a loading text
+         */
+        dispatch({type: "FETCH_GIT"});
+        /*
+           search the git with the REST API
+        */
+        fetch("https://api.github.com/search/code?q="+urlParams)
             .then(response => response.json())
             .then((response) => {
                 dispatch({type: "FETCH_GIT_FULFILLED", payload: response})
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react"
 import { connect } from "react-redux"
 import {bindActionCreators} from 'redux'
 import Time from 'react-time'
-import { fetchGit } from "../actions/fetchActions"
+import { fetchGitProject, fetchGitCode } from "../actions/fetchActions"
 
 
 class Layout extends React.Component {
@@ -10,26 +10,43 @@ class Layout extends React.Component {
     constructor(props) {
         super(props);
 
-        this.fetchgit = this.fetchgit.bind(this);
+
     //todo: to move to an i18n state
         this._text = {
+            textPlaceHolder: "text",
             languagePlaceHolder: "language",
-            topicplaceHolder:"topic"
+            topicPlaceHolder:"topic",
+            repoPlaceHolder:"repo"
         }
     }
 
     /**
-     * function to call the fetch method
+     * function to call the fetch method to search Project
      */
-    fetchgit() {
+    fetchGitProject() {
         //todo: move this to the state and use it from there.
         var sQuerry = {
+            text : this.refs.searchText.value,
             lang  : this.refs.searchTextLang.value,
             topic : this.refs.searchTextTopic.value
         }
 
-        this.props.actions(sQuerry)
+        this.props.fetchGitProject(sQuerry)
     }
+    /**
+     * function to call the fetch method to search Project's code
+     */
+    fetchGitCode() {
+        //todo: move this to the state and use it from there.
+        var sQuerry = {
+            text : this.refs.searchText.value,
+            lang  : this.refs.searchTextLang.value,
+            topic : this.refs.searchTextTopic.value
+        }
+
+        this.props.fetchGitCode(sQuerry)
+    }
+
 
     /**
      * function that will repeated to render the git project details
@@ -37,18 +54,33 @@ class Layout extends React.Component {
      * @returns {*}
      */
     renderStat(stat) {
-        let now = new Date(stat.updated_at.split("T")[0])
+        let now = ""
+        if(stat && stat.updated_at){
+            now = new Date(stat.updated_at.split("T")[0])
+        }
+        let informationText =  ""
+        if(now !== ""){
+            informationText =    <span>
+                        <div className="information">Open Issues count : {stat.open_issues_count}</div>
+                        <div className="information">watchers : {stat.watchers_count}</div>
+                        <div className="information">Last Updated : <Time value={now} format="MMM DD YYYY" /> </div>
+                    </span>
+
+        } else{
+            informationText = <span>
+                        <div className="information">Path : {stat.url}</div>
+                        <div className="information">Score : {stat.score}</div>
+                    </span>
+
+        }
+
         return (
             <li key={stat.name} >
-                <img className="user-info__avatar" src={stat.owner.avatar_url} alt={stat.name} />
+                <img className="user-info__avatar" src={stat.owner ? stat.owner.avatar_url : stat.repository.owner.avatar_url} alt={stat.name} />
                 <div className="project-name">
-                    {stat.full_name}
+                    {stat.full_name ? stat.full_name : stat.path}
                 </div>
-                <div className="information">Open Issues count : {stat.open_issues_count}</div>
-                <div className="information">watchers : {stat.watchers_count}</div>
-
-                <div className="information">Last Updated : <Time value={now} format="MMM DD YYYY" /> </div>
-
+                {informationText}
             </li>
         );
     }
@@ -57,22 +89,28 @@ class Layout extends React.Component {
 
         const  gits = this.props.gitProject;
         //render the search form when there is no data
-        if (!gits) {
+        if (!gits || gits.errors) {
+            let errorMsg = "";
+            if(gits && gits.errors){
+                errorMsg = <div className='errorMsg'> <h4>{gits.message}</h4><div>{gits.errors[0].message} </div> </div>
+            }
             return (
                 <div>
+                    {errorMsg}
                     <span ref="projectSearchContainer" >
-
+                        <input ref="searchText" placeholder={this._text.textPlaceHolder} className="search-page__input" type="text" />
                         <input ref="searchTextLang" placeholder={this._text.languagePlaceHolder} className="search-page__input" type="text" />
-
-                        <input ref="searchTextTopic" placeholder={this._text.topicplaceHolder} className="search-page__input" type="text" />
-
+                        <input ref="searchTextTopic" placeholder={this._text.topicPlaceHolder} className="search-page__input" type="text" />
+                        <input ref="searchTextTopic" placeholder={this._text.repoPlaceHolder} className="search-page__input" type="text" />
                     </span>
                     <div ref="buttonSearchContainer" >
-                        <button onClick={this.fetchgit.bind(this)} className="search-page__button">Search</button>
+                        <button onClick={this.fetchGitProject.bind(this)} className="search-page__button">Search Projects</button>
+                        <button onClick={this.fetchGitCode.bind(this)} className="search-page__button">Search Code</button>
                     </div>
                 </div>
             )
         }
+
         // render the result list
         // todo:  create a goback logic to return to the search page.
         return (
@@ -90,6 +128,7 @@ export default connect(state => ({
         gitProject: state.gits
     }),
     (dispatch) => ({
-        actions: bindActionCreators(fetchGit, dispatch)
+        fetchGitProject: bindActionCreators(fetchGitProject,dispatch),
+        fetchGitCode : bindActionCreators(fetchGitCode, dispatch)
     })
 )(Layout)
